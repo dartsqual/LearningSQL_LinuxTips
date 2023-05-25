9c753723d26ad8a3b489ff51fd1351fc735daaec
Igor Pereira Anselmo <dartsqual@gmail.com>
2023-05-25 20:21:25 -0300

    Day 2 end

--- dartsqual/Dia2 - Count.sql
+++ dartsqual/Dia2 - Count.sql
@@ -14,12 +14,51 @@ WHERE flKetchup IS NOT NULL
 
 -- COMMAND ----------
 
-SELECT *
+SELECT descUF,
+        count(*) AS qtdePedidos
 
 FROM silver.pizza_query.pedido
 
-WHERE descUF = "Paraná"
+WHERE descUF != 'São Paulo' -- Pode usar Not Like mas a performance é menor
+
+GROUP BY descUF
+ORDER BY qtdePedidos DESC 
+
+LIMIT 6
 
 -- COMMAND ----------
 
+SELECT *
+FROM silver.pizza_query.produto
+WHERE descItem LIKE 'tofu'
+
+-- COMMAND ----------
+
+-- Usando Having para filtrar por quantidade de pedidos
+SELECT descUF,
+        count(*) AS qtdePedidos
+
+FROM silver.pizza_query.pedido
+
+WHERE descUF != 'São Paulo'
+
+GROUP BY descUF
+HAVING qtdePedidos >= 75
+
+ORDER BY qtdePedidos DESC 
+
+LIMIT 6
+
+-- COMMAND ----------
+
+SELECT descUF,
+       flKetchup,
+       count(*)
+FROM silver.pizza_query.pedido
+
+GROUP BY descUF, flKetchup
+ORDER BY descUF, flKetchup
+
+
+-- COMMAND ----------
 
