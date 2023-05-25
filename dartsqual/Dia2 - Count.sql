9de8bbbfaeb1213a9877d13f7059ea7dfcbec3d2
Igor Pereira Anselmo <dartsqual@gmail.com>
2023-05-25 19:54:18 -0300

    Update: LICENSE README.md dartsqual

--- dartsqual/Dia2 - Count.sql
+++ dartsqual/Dia2 - Count.sql
@@ -1,6 +1,25 @@
 -- Databricks notebook source
-SELECT  count(*)
+SELECT  count(*),       -- linhas não nulas
+        count(1),       -- linhas não nulas tbm
+        count(idPedido) -- linhas que tem idPedido
 FROM silver.pizza_query.pedido
 
 -- COMMAND ----------
+
+SELECT count(*)
+
+FROM silver.pizza_query.pedido
+
+WHERE flKetchup IS NOT NULL
+
+-- COMMAND ----------
+
+SELECT *
+
+FROM silver.pizza_query.pedido
+
+WHERE descUF = "Paraná"
+
+-- COMMAND ----------
+
 
