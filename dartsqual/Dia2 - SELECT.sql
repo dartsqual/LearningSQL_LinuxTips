4415ba2c60ce23acd165e96195fcf3a9d44554ae
Igor Pereira Anselmo <dartsqual@gmail.com>
2023-05-25 19:35:26 -0300

    Learned

--- dartsqual/Dia2 - SELECT.sql
+++ dartsqual/Dia2 - SELECT.sql
@@ -1,1 +1,13 @@
 -- Databricks notebook source
+SELECT * 
+FROM silver.pizza_query.item_pedido
+
+-- COMMAND ----------
+
+-- Aprendendo a usar o Select
+
+SELECT descItem, vlPreco, 21 + 21 AS vl42
+FROM silver.pizza_query.produto
+
+-- COMMAND ----------
+
